8bb80d3453ea5fc2dc586237d42f00fc0cba899d
MihaMuskinja <miha.muskinja@gmail.com>
2017-05-17 13:58:48 +0000

    started working on grid submit

---
+++ batch/GridScript.sh
@@ -0,0 +1,4 @@
+find .
+echo $IN
+echo $IN2
+echo $IN3
---
+++ batch/SubmitGrid.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+'''
+SubmitHist.py
+'''
+import ROOT
+
+## modules
+import os
+import re
+import subprocess
+import time
+from   ssdilep.samples import samples
+
+jobName = "user.mmuskinj.testFederico123"
+
+cmd = "prun --exec batch/GridScript.sh"
+cmd+= " --inDS user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_tree.root"
+cmd+= " --secondaryDSs IN2:1:user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_metadata.root,IN3:1:user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_cutflow.root"
+cmd+= " --nFilesPerJob 1"
+cmd+= " --extFile ssdilep/data/chargeFlipRates-28-03-2017.root,ssdilep/data/fakeFactor-16-05-2017.root"
+cmd+= " --outDS " + jobName
+
+print cmd
+
+m = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
+print m.communicate()[0]
