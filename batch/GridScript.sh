f26f5d43f18b6409aeb0cd83f93054d8479e93a9
MihaMuskinja <miha.muskinja@gmail.com>
2017-05-17 14:51:51 +0000

    building the grid script

--- batch/GridScript.sh
+++ batch/GridScript.sh
@@ -2,3 +2,6 @@ find .
 echo $IN
 echo $IN2
 echo $IN3
+
+python ssdilep/run/j.plotter_ele_allR.py --input $IN --sampletype="mc" --config="min_entry:0,max_entry:-1,sys:EG_SCALE_LARTEMPERATURE_EXTRA2016PRE_DN"
+hadd out.root ntuple.root $IN2 $IN3
