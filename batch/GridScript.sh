14b5aa57155f280cc5cd5daa2cbafca59a23a308
MihaMuskinja <miha.muskinja@gmail.com>
2017-05-18 08:22:25 +0200

    latest grid script

--- batch/GridScript.sh
+++ batch/GridScript.sh
@@ -1,7 +1,13 @@
 find .
-echo $IN
-echo $IN2
-echo $IN3
+echo $1
+echo $2
+echo $3
 
-python ssdilep/run/j.plotter_ele_allR.py --input $IN --sampletype="mc" --config="min_entry:0,max_entry:-1,sys:EG_SCALE_LARTEMPERATURE_EXTRA2016PRE_DN"
-hadd out.root ntuple.root $IN2 $IN3
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh --quiet
+lsetup root
+
+source setup.sh
+
+python ssdilep/run/j.plotter_ele_allR.py --input $1 --sampletype="mc" --config="min_entry:0,max_entry:-1,sys:EG_SCALE_LARTEMPERATURE_EXTRA2016PRE_DN"
+hadd out.root ntuple.root $2 $3
--- batch/SubmitGrid.py
+++ batch/SubmitGrid.py
@@ -11,17 +11,17 @@ import subprocess
 import time
 from   ssdilep.samples import samples
 
-jobName = "user.mmuskinj.testFederico124"
+jobName = 'user.mmuskinj.testFederico127'
 
-cmd = "prun --exec batch/GridScript.sh"
-cmd+= " --inDS user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_tree.root"
-cmd+= " --secondaryDSs IN2:1:user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_metadata.root,IN3:1:user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_cutflow.root"
-cmd+= " --nFilesPerJob 1"
-cmd+= " --extFile ssdilep/data/chargeFlipRates-28-03-2017.root,ssdilep/data/fakeFactor-16-05-2017.root"
-cmd+= " --excludeFile run"
-cmd+= " --mergeOutput"
-cmd+= " --outputs out.root"
-cmd+= " --outDS " + jobName
+cmd = 'prun --exec "batch/GridScript.sh %IN %IN2 %IN3"'
+cmd+= ' --inDS user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_tree.root'
+cmd+= ' --secondaryDSs IN2:1:user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_metadata.root,IN3:1:user.mmuskinj.SSDiLep.EX12MC.v3.sys.002.410069.MadGraphPythia8EvtGen_A14NNPDF23LO_ttZllonshell_Np0_cutflow.root'
+cmd+= ' --nFilesPerJob 1'
+cmd+= ' --extFile ssdilep/data/chargeFlipRates-28-03-2017.root,ssdilep/data/fakeFactor-16-05-2017.root'
+cmd+= ' --excludeFile run'
+cmd+= ' --mergeOutput'
+cmd+= ' --outputs out.root'
+cmd+= ' --outDS ' + jobName
 
 print cmd
 
